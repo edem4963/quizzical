96ca02568a2e1efcce45df4726970a7693fa3276
edem4963 <112492837+edem4963@users.noreply.github.com>
2023-09-20 16:33:55 -0400

    first commit with basic css styling for app

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='intro--page'>
+        <h1>Quizzical</h1>
+        <p>Some description if needed</p>
+        <button className='button start'>Start Quiz</button>
+      </div>
+    </>
+  )
+}
+
+export default App
